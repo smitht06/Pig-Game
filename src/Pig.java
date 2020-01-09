c41c8c3c333a0d55fa031037c9208c0057b466b0
Anthony Smith <as361@students.uwf.edu>
2020-01-09 09:07:53 -0500

    created random method

---
+++ src/Pig.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Pig {
+    public static void main(String [] args){
+        
+    }
+
+    private static int random(){
+        int diceRoll = (int)(7 * Math.random());
+        return diceRoll;
+    }
+}
