5b5a0963699baa7b14a700d4fc4483b81e9b31a6
Anthony Smith <as361@students.uwf.edu>
2020-01-09 09:27:49 -0500

    added basic roll logic

--- src/Pig.java
+++ src/Pig.java
@@ -1,8 +1,24 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Pig {
     public static void main(String [] args){
-        
+        Scanner scnr = new Scanner(System.in);
+        char choice = 'q';
+        int score = 0;
+        System.out.println("Welcome to the Pig game!");
+        while(choice != 's'){
+            System.out.println("Enter r to roll or s to stop.");
+            choice = scnr.next().charAt(0);
+            if(choice == 'r'){
+                int roll = random();
+                System.out.println("You rolled a " + roll);
+                score += roll;
+                System.out.println("Your turn score is: " + roll + " if you stop now your total score will be " + roll);
+            }
+        }
+
+
     }
 
     private static int random(){
