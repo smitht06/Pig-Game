c75ed4a4b0e8381f90693c6a30856c28ac3d79f0
Anthony Smith <as361@students.uwf.edu>
2020-01-15 08:22:14 -0500

    completed comments

--- src/Pig.java
+++ src/Pig.java
@@ -37,10 +37,12 @@ public class Pig {
                 //enter r to roll and s to hold
                 System.out.println("Enter r to roll or s to hold.");
                 choice = scnr.next().charAt(0);
+                //if player rolls the random method is used with the provided seed, program shows user their roll and turn score
                 if(choice == 'r'){
                     int roll = random(seed);
                     System.out.println("\nYou rolled a " + roll);
                     userTurnScore += roll;
+                    //if user rolls a 1 their turn is over and turn score is not added to total score
                     if(roll == 1){
                         System.out.println("Your turn is over, your current score is " + userTotalScore);
                         userTurnScore = userTurnScore-1;
@@ -54,29 +56,35 @@ public class Pig {
                     break;
                 }
             }
+
+            //computer has a turn if the users total score is less than score to win
             if(userTotalScore < scoreToWin){
                 System.out.println("\nComputer's turn:");
+                //while loop for computer's turn
                 while(computerTotalScore <= scoreToWin){
-
                     int computerRoll = random(seed);
                     System.out.println("Computer rolled: " + computerRoll);
                     computerTurnScore += computerRoll;
+                    //computer's turn is over if 1 is rolled
                     if(computerRoll == 1){
                         System.out.println("Computer rolled a 1.");
                         computerTurnScore = 0;
                         break;
+                    //computer will hold and pass turn back to user if their turn score is 20 or above
                     }else if(computerTurnScore >= 20){
                         computerTotalScore += computerTurnScore;
                         System.out.println("Computer holds at: " + computerTotalScore);
                         computerTurnScore = 0;
                         break;
                     }
+                    //computer holds if winning score is reached
                     if (computerTurnScore + computerTotalScore >= scoreToWin){
                         computerTotalScore += computerTurnScore;
                         System.out.println("Computer holds at: " + computerTotalScore);
                         break;
                     }
                 }
+                //if statement if computer wins
                 if(computerTotalScore >= scoreToWin){
                     System.out.println("computer wins!");
                     break;
@@ -87,6 +95,7 @@ public class Pig {
             }
         }
     }
+        //method for a random, seeded dice roll
         private static int random(int seed){
             Random diceRoll = new Random();
             int result = diceRoll.nextInt(6-1+1)+1;
